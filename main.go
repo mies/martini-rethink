b43ed2a9175cd5fabb9fd8af2f5a4aeb8f366437
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2014-02-03 13:57:59 +0100

    post

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"log"
+"log"
 	//"time"
-	"github.com/codegangsta/martini"
-	"github.com/codegangsta/martini-contrib/render"
-	"github.com/codegangsta/martini-contrib/binding"
-	rethink "github.com/dancannon/gorethink"
+"github.com/codegangsta/martini"
+"github.com/codegangsta/martini-contrib/render"
+"github.com/codegangsta/martini-contrib/binding"
+rethink "github.com/dancannon/gorethink"
 )
 
 type Todo struct {
@@ -16,32 +16,41 @@ type Todo struct {
 func main() {
 	session := InitDB()
 
-    m := martini.Classic()
-    m.Use(render.Renderer())
+	m := martini.Classic()
+	m.Use(render.Renderer())
 
-    m.Get("/todos", func(r render.Render)  {
+	m.Get("/todos", func(r render.Render) {
+		result := []Todo{}
 		rows, err := rethink.Table("todos").Run(session)
+		//rows, err := query.Run(session)
 		if err != nil {
 			log.Println(err)
 		}
 
-		log.Println(rows)
+		for rows.Next() {
+			var t Todo
+			err := rows.Scan(&t)
+			if err != nil {
+				log.Println(err)
+			}
+			result = append(result, t)
+		}
+		//r.JSON(200, map[string]interface{}{"hello": "world"})
+		r.JSON(200, result)
+		})
 
-		r.JSON(200, map[string]interface{}{"hello": "world"})
-    })
-
-	m.Post("/todos", binding.Form(Todo{}), func(todo Todo, r render.Render) {
-
+	m.Post("/todos", binding.Json(Todo{}), func(todo Todo, r render.Render) {
 		log.Println(todo)
 		_, err := rethink.Table("todos").Insert(todo).RunWrite(session)
 		if err != nil {
 			log.Println(err)
+			return
 		}
 
-		r.JSON(200, map[string]interface{}{"yes":"sir"})
+		r.JSON(200, map[string]interface{}{"todo added":"ok"})
 
-	})
+		})
+	  
 
-
-    m.Run()
+	m.Run()
 }
