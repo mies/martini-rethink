191c07658b5a647e216da5ca34cbd857ba1e4cac
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2014-02-07 14:16:44 +0100

    json matching

--- main_test.go
+++ main_test.go
@@ -8,7 +8,7 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-    . "github.com/onsi/gomega/matchers"
+    //. "github.com/onsi/gomega/matchers"
 )
 var _ = Describe("Todo", func() {
 
