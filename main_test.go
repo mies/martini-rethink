ea7e9564ccf4aa3c7074e74bf0c82bde3c82db93
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2014-02-07 12:48:17 +0100

    simplejson

--- main_test.go
+++ main_test.go
@@ -4,35 +4,70 @@ import (
     "encoding/json"
     "log"
     "bytes"
+    //"io/ioutil"
+    "github.com/bitly/go-simplejson"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
-
-var _ = Describe("List all todos", func() {
-	It("returns a 200 Status Code", func() {
-		Request("GET", "/todos", HandleIndex)
-		Expect(response.Code).To(Equal(200))
-	})
-})
-
-var _ = Describe("Insert new todo", func() {
+var _ = Describe("Todo", func() {
 
     var (
+         js *simplejson.Json
         body []byte
         err error
+        //todos []Todo
     )
 
-    BeforeEach(func() {
-        todo := Todo{"keep things green"}
-        body, err = json.Marshal(todo)
-        if err != nil {
-            log.Println("Unable to marshal todo")
-        }
+    Describe("List all todos", func() {
+        It("returns a 200 Status Code", func() {
+            Request("GET", "/todos", HandleIndex)
+            Expect(response.Code).To(Equal(200))
+        })
     })
 
-    It("returns a 200 Status Code", func() {
-        PostRequest("POST", "/todos", HandleNewTodo, bytes.NewReader(body))
-        Expect(response.Code).To(Equal(200))
+    Describe("Create a Todo", func() {
+
+        BeforeEach(func() {
+            todo := Todo{"keep things green"}
+            body, err = json.Marshal(todo)
+            if err != nil {
+                log.Println("Unable to marshal todo")
+            }
+        })
+
+        It("returns a 200 Status Code", func() {
+            PostRequest("POST", "/todos", HandleNewTodo, bytes.NewReader(body))
+            Expect(response.Code).To(Equal(200))
+        })
+    })
+
+    Describe("Returns a single created todo", func() {
+        It("returns a 200 Status Code", func() {
+            Request("GET", "/todos", HandleIndex)
+            log.Println(response)
+            log.Println(response.Body)
+            Expect(response.Code).To(Equal(200))
+            js, err = simplejson.NewJson([]byte(response.Body.String()))
+
+            log.Println(js)
+
+
+            log.Println(js.GetIndex(1).Get("Name"))
+
+
+            /*body, err = ioutil.ReadAll(response.Body)
+            log.Println("=====")
+            log.Println(body)
+            err = json.Unmarshal(body, &todos)
+              log.Println(err)
+            log.Println(todos)
+            */
+        })
     })
 })
+
+
+
+
+
 
